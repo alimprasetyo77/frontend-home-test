5494a108813c054275d960c5ef26cb174784b6ed
alimprasetyo77 <palim695@gmail.com>
2025-07-15 04:25:38 +0700

    refactor : next config

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig: import("next").NextConfig = {
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
   images: {
     remotePatterns: [
       {
@@ -13,4 +15,4 @@ const nextConfig: import("next").NextConfig = {
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
